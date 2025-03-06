49a573f85be06af9a61478f81b8f4c63b343f800
kutay <mehmet.bozkurt@kcl.ac.uk>
2025-03-06 12:06:23 +0000

    style(src): improve code style just a tad bit

--- src/App.java
+++ src/App.java
@@ -1,54 +1,27 @@
-/*
- * Copyright (c) 2018, 2021, Gluon
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL GLUON BE LIABLE FOR ANY
- * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
-
-import com.gluonhq.maps.MapLayer;
 import com.gluonhq.maps.MapPoint;
 import com.gluonhq.maps.MapView;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-import javax.xml.parsers.SAXParser;
-
+/**
+ * Main App class for the "England is my Polluted City" project. This class
+ * creates a JavaFX application that can display a map of UK with pollution
+ * data. It centres the map on London.
+ * 
+ * @author Anas Ahmed, Mehmet Kutay Bozkurt, Matthias Loong, and Chelsea Feliciano
+ * @version 1.0
+ */
 public class App extends Application {
     @Override
     public void start(Stage stage) {
         MapView mapView = new MapView();
-        //mapView.addLayer(positionLayer());
         mapView.addLayer(new PollutionLayer(mapView));
         mapView.setZoom(14);
 
-
-        stage.setTitle("Gluon Maps Demo");
+        stage.setTitle("England is my Polluted City");
 
         StackPane root = new StackPane();
         root.getChildren().add(mapView);
@@ -57,20 +30,8 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
 
-        MapPoint startPosition = new MapPoint(51.508045, -0.128217); //coordinates for trafalgar square
-        mapView.flyTo(0., startPosition, 0.1); //Spawns map instantly
-    }
-
-    private MapLayer positionLayer() {
-        PoiLayer answer = new PoiLayer();
-        answer.addPoint(new MapPoint(51.508045, -0.128217), new Circle(7, Color.RED));
-
-        Circle circle = new Circle(30, Color.YELLOW); // Large radius to show
-        circle.setOpacity(0.4); // Transparency to blend colors
-        MapPoint pollutionPoint = new MapPoint(51.51275, -0.117278); //coordinates for bush house
-        //answer.addPoint(pollutionPoint, circle);
-
-        return answer;
+        MapPoint startPosition = new MapPoint(51.508045, -0.128217); // These are the coordinates for Trafalgar Square.
+        mapView.flyTo(0., startPosition, 0.1); // Instantly opens on top London.
     }
 
     public static void main(String[] args) {
--- src/PollutionLayer.java
+++ src/PollutionLayer.java
@@ -1,18 +1,26 @@
 import com.gluonhq.maps.MapLayer;
 import com.gluonhq.maps.MapPoint;
 import com.gluonhq.maps.MapView;
+
 import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 
 /**
- * TODO remove or refactor
- * Test class for pollution rendering
+ * TODO: remove or refactor
+ * Class for pollution Rendering on the map.
+ * 
+ * @author Anas Ahmed, Mehmet Kutay Bozkurt, Matthias Loong, and Chelsea Feliciano
+ * @version 1.0
  */
 public class PollutionLayer extends MapLayer {
     private final MapView mapView;
     private final Circle circle;
 
+    /**
+     * Constructor for PollutionLayer.
+     * @param mapView The map view to render the pollution layer on.
+     */
     public PollutionLayer(MapView mapView) {
         this.mapView = mapView;
         circle = new Circle(30, Color.RED);
@@ -20,18 +28,21 @@ public class PollutionLayer extends MapLayer {
         this.getChildren().add(circle);
     }
 
+    /**
+     * Method to update the layout for pollution.
+     */
     @Override
     protected void layoutLayer() {
-        MapPoint point = new MapPoint(51.51275, -0.117278); //Point to draw circle
+        MapPoint point = new MapPoint(51.51275, -0.117278); // Point to draw circle -- Bush House
         Point2D mapPoint = getMapPoint(point.getLatitude(), point.getLongitude());
         circle.setVisible(true);
         circle.setTranslateX(mapPoint.getX());
         circle.setTranslateY(mapPoint.getY());
 
-        circle.setRadius(30 * mapView.getZoom()/14);
+        circle.setRadius(30 * mapView.getZoom() / 14);
 
-        //Experimenting with how to get an accurate scale value to use, converting pixel size to real world size
-        //WIP TODO
+        // Experimenting with how to get an accurate scale value to use, converting pixel size to real world size.
+        // TODO: Work on this.
         MapPoint A = mapView.getMapPosition(0, 0);
         MapPoint B = mapView.getMapPosition(1, 1);
         double xScale = A.getLatitude() - B.getLatitude();
