1374f0304384772aa58fc4874c80cba3629f7ad2
kutay <mehmet.bozkurt@kcl.ac.uk>
2025-03-09 10:56:50 +0000

    chore(src): remove DistanceTracker and clean up unused code

--- src/App.java
+++ src/App.java
@@ -24,11 +24,8 @@ public class App extends Application {
         MapView mapView = new MapView();
         PollutionLayer pollutionLayer = new PollutionLayer(mapView);
 
-        // DistanceTracker distanceTracker = new DistanceTracker();
 
         mapView.addLayer(pollutionLayer);
-        // mapView.addLayer(distanceTracker);
-
         mapView.setZoom(14);
 
         stage.setTitle("England is my Polluted City");
@@ -40,13 +37,8 @@ public class App extends Application {
         stage.setScene(scene);
         stage.show();
 
-        // scene.setOnMouseClicked((MouseEvent event) -> {
-        //     distanceTracker.addNode(mapView.getMapPosition(event.getX(), event.getY()));
-        // });
-
         MapPoint startPosition = new MapPoint(51.508045, -0.128217); // These are the coordinates for Trafalgar Square.
-        // MapPoint startPosition = new MapPoint(60.854303762303054, -0.8863583375656637);
-        mapView.flyTo(0., startPosition, 0.01); // Instantly opens on top of London.
+        mapView.flyTo(0, startPosition, 0.01); // Instantly opens on top of London.
     }
 
     public static void main(String[] args) {
