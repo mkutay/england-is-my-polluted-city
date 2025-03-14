2e937e5e51fec9b78b40067c7752a7833fcbd9ed
k22042916 <k22042916@kcl.ac.uk>
2025-03-14 07:52:54 +0000

    refactor(DefaultColorScheme) - change colours and added toString for UI display names

--- src/colors/DefaultColorScheme.java
+++ src/colors/DefaultColorScheme.java
@@ -15,12 +15,20 @@ public class DefaultColorScheme extends ColorScheme {
     public DefaultColorScheme() {
         //colors = List.of(Color.GREEN, Color.YELLOW, Color.RED, Color.rgb(128, 0, 128));
         colors = List.of(
-                Color.rgb(53, 215, 0),
-                Color.rgb(255, 220, 0),
-                Color.rgb(255, 20, 60),
-                Color.rgb(115, 0, 120),
-                Color.rgb(0, 0, 0)
+                Color.rgb(0, 128, 0),    // Green (Low emissions)
+                Color.rgb(255, 215, 0),  // Yellow (Moderate emissions)
+                Color.rgb(255, 165, 0),  // Orange (High emissions)
+                Color.rgb(255, 69, 0),   // Red (Very high emissions)
+                Color.rgb(128, 0, 0)     // Dark Red (Severe emissions)
         );
+    }
 
+    /**
+     * Overrides the default toString() method to display a user-friendly name.
+     * @return A string representing the name of this color scheme.
+     */
+    @Override
+    public String toString() {
+        return "Default";
     }
 }
