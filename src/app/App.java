c682be2335141216d8988bf3b79f7a57335fd9a8
Anas Ahmed <70797303+robot1273@users.noreply.github.com>
2025-03-16 01:05:20 +0000

    hotfix : re-enabled welcome page

--- src/app/App.java
+++ src/app/App.java
@@ -32,7 +32,7 @@ public class App extends Application {
         stage.show();
 
         //Show welcome page on launch
-        //new WelcomePageController().show();
+        new WelcomePageController().show();
     }
 
     public static void main(String[] args) {
